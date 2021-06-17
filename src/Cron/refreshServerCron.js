80e3b28f9d58e443f56e128fdbda99282835f90c
Victor Atavila <victoratavila@hotmail.com>
2021-06-17 18:18:13 -0300

    Bug fixing

--- src/Cron/refreshServerCron.js
+++ src/Cron/refreshServerCron.js
@@ -1,10 +1,9 @@
 const cron = require('node-cron');
 const axios = require('axios');
-const PORT = process.env.PORT;
 
 if(process.env.PROD == true){
-    cron.schedule('*/10 * * * *', () => {
-        const url = `http://localhost:${PORT}/status`;
+    cron.schedule('* * * * *', () => {
+        const url = 'https://currencycoverter-api.herokuapp.com/status'
         console.log('running a task every minute');
 
         axios.get(url).then((result) => {
