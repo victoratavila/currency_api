98687db0e88043dee1a9c936e4d180cc9ae794dc
Victor Atavila <victoratavila@hotmail.com>
2021-09-19 22:41:55 -0300

    Report generating logic changed

--- src/Controllers/Reports.js
+++ src/Controllers/Reports.js
@@ -6,35 +6,73 @@ const percentage = require('calculate-percentages');
 module.exports = {
     async compareValues(req, res){
 
-        let results = {}
+        let process = {}
+        let results = {};
 
         await currency.findAll({
-            attributes: ['currency', 'value', 'lastUpdate', 'slug']
+            attributes: ['currency', 'value', 'lastUpdate', 'slug'],
+            raw: true
          }).then(currencies => {
 
            previousdayvalues.findAll({
-            attributes: ['currency', 'value', 'lastUpdate', 'slug']
+            attributes: ['currency', 'value', 'lastUpdate', 'slug'],
+            raw: true
            }).then(yerterdayCurrencies => {
 
-            for(let i = 0; i < currencies.length; i++){
+            // for(let i = 0; i < currencies.length; i++){
       
-              let yesterdayValue = currencies[i].dataValues.value;
-              let todayValue = yerterdayCurrencies[i].dataValues.value;
+            //   let yesterdayValue = currencies[i].dataValues.value;
+            //   let todayValue = yerterdayCurrencies[i].dataValues.value;
 
-              let currencyName = currencies[i].dataValues.slug;
+            //   let currencyName = currencies[i].dataValues.slug;
 
-              if(todayValue > yesterdayValue){
-                  let difference = (percentage.differenceBetween(todayValue, yesterdayValue)).toFixed(0);
+            //   if(todayValue > yesterdayValue){
+            //       let difference = (percentage.differenceBetween(todayValue, yesterdayValue)).toFixed(0);
 
-                  results[currencyName] = `${difference}%`;
+            //       results[currencyName] = `${difference}%`;
 
-              } else {
-                let difference = (percentage.differenceBetween(todayValue, yesterdayValue)).toFixed(0);
+            //   } else {
+            //     let difference = (percentage.differenceBetween(todayValue, yesterdayValue)).toFixed(0);
 
-                results[currencyName] = `${difference}%`;
-              }
+            //     results[currencyName] = `${difference}%`;
+            //   }
+            // }
+
+            async function searchYesterday(){
+
+              await yerterdayCurrencies.forEach(yesterday => {
+                let yesterdayValue = yesterday.value;
+                let slug = yesterday.slug;
+
+                if(process[slug] != undefined){
+               
+                   let todayValue = process[slug];
+ 
+                   const difference = percentage.differenceBetween(yesterdayValue, todayValue).toFixed(2);
+         
+                  results[slug] = difference;
+
+                }
+               
+
+              })
+
             }
 
+          
+            currencies.forEach(( today => {
+
+              let todayValue = today.value;
+              let slug = today.slug;
+
+              // Storing temporarely the value in the object results
+               process[slug] = todayValue;
+               searchYesterday();
+          }))
+
+
+
+    
             res.json(results)
            }).catch(err => {
                console.log(err);
