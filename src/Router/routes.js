2790d3df20c7d4942c6f877ad0948d92986394e4
Victor Atavila <victoratavila@hotmail.com>
2021-10-26 18:42:56 -0300

    Core testing

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -12,8 +12,8 @@ const CronStorePreviousDayValues =  require('../Cron/storePreviousDayCurrencyVal
 const cors = require('cors');
 
 var corsOptions = {
-    origin: ['http://localhost:3000'],
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: 'http://localhost:7182',
+    optionsSuccessStatus: 200
   }
    
 
