6aabfc4bc9efe71549a120df365e5a94e426dd3e
Victor Atavila <victoratavila@hotmail.com>
2021-10-26 22:40:47 -0300

    Changes undone

--- src/app.js
+++ src/app.js
@@ -2,18 +2,17 @@ const express = require('express');
 const app = express();
 const connection = require('./database/connection');
 const routes = require('./Router/routes');
-// const cors = require('cors');
+const cors = require('cors');
 
-// const corsOptions = {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200,
-//     methods: "GET, PUT, POST, DELETE"
+
+// if(process.env.PROD == undefined){
+//     var PORT = 8080
+// } else {
+//     var PORT = process.env.PORT;
 // }
 
-// // Setting cors
-// app.use(cors(corsOptions));
-
-
+// Setting cors
+app.use(cors());
 
 // Authenticate database connection
 connection.authenticate().then(() => {
@@ -26,8 +25,6 @@ app.use(express.urlencoded({ extended: true }))
 
 // Setting static folder
 app.use(express.static(__dirname + '/public'));
-
-
 
 // Setting routes
 app.use(routes);
