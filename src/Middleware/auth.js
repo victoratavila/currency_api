c64411d5c48c39156842bb1cbc9f3e5505d7143e
Victor Atavila <victoratavila@hotmail.com>
2021-10-31 21:32:47 -0300

    Newsletter subscription logic added

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const { secret } = process.env;
+const { secret, GENERAL_TOKEN } = process.env;
 
 const authAdmin = (req, res, next) => {
     const token = req.headers.token;
 
+    // if(token != undefined && token != "" && token != null){
+    //     jwt.verify(token, secret, (err, data) => {
+    //         if(err){
+    //            
+    //         } else {
+    //             next();
+    //         }
+    //     });
+    // } else {
+    //     res.status(403).json({error: 'Please provide a token to access this resource'});
+    // }
+
     if(token != undefined && token != "" && token != null){
-        jwt.verify(token, secret, (err, data) => {
-            if(err){
-                res.status(403).json({error: 'Please provide a valid token to access this resource'});
-            } else {
-                next();
-            }
-        });
+        if(token == GENERAL_TOKEN){
+            next();
+        } else {
+            res.status(403).json({error: 'Access denied. Please provide a valid token to access this resource'});
+        }
     } else {
-        res.status(403).json({error: 'Please provide a token to access this resource'});
+            res.status(400).json({error: 'Please provide a token to access this resource'})
     }
+
+    
 
 }
 
