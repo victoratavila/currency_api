ceeef7e819106fff935e2995a472b9329d34b263
Victor Atavila <victoratavila@hotmail.com>
2021-11-26 15:33:36 -0300

    Calculator BRL currency applied

--- src/Controllers/Calculator.js
+++ src/Controllers/Calculator.js
@@ -46,8 +46,8 @@ module.exports = {
                                     details: {
                                         "converted_from": from_currency_code.toUpperCase(),
                                         "converted_from_symbol": from_currency_symbol,
-                                        "to_currency": to_currency_code.toUpperCase(),
-                                        "to_currency_code": to_currency_symbol,
+                                        "converted_to_code": to_currency_code.toUpperCase(),
+                                        "converted_to_symbol": to_currency_symbol,
                                         "value_converted": from_currency_value
                                     }
                                 })
@@ -61,5 +61,56 @@ module.exports = {
 
         })
 
+    },
+
+    async realCalculator(req, res){
+
+        const { convert_from_amount, to_currency_code, inverse } = req.body;
+
+        if(convert_from_amount == undefined || to_currency_code == undefined){
+            res.status(400).json({error: 'Please inform amount to convert from and currency destiny code to convert'});
+        } else {
+            await Currency.findOne({
+                where: {
+                    code: to_currency_code
+                }
+            }).then(result => {
+                if(result){
+
+                    if(inverse == undefined || inverse == false){
+                        const data = {
+                            result: (convert_from_amount/result.value).toFixed(2),
+                            details: {
+                                converted_amount: convert_from_amount,
+                                converted_to_code: result.code,
+                                converted_to_symbol: result.symbol,
+                                currency_value_per_real: result.value,
+                                BRL_to_currency: true
+                            }
+                        }
+    
+                        res.json(data);
+                    } else {
+                        const data = {
+                            result: (result.value * convert_from_amount).toFixed(2),
+                            details: {
+                                converted_amount: convert_from_amount,
+                                converted_from_code: result.code,
+                                converted_from_symbol: result.symbol,
+                                currency_value_per_real: result.value,
+                                currency_to_BRL: true
+                            }
+                        }
+    
+                        res.json(data);
+                    }
+                 
+                } else {
+                    res.status(404).json({error: `Currency not found related to code ${to_currency_code}, please inform a valid currency code`});
+                }
+            }).catch(err => {
+                console.log(err);
+            })
+        }
     }
 }
